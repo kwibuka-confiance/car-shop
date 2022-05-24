326ea14f9127b9b26bc16170d3c563f77637a26e
Kwibuka Confiance <swiftmotion2020@gmail.com>
2022-05-24 10:50:23 +0200

    adding class

--- app/app.js
+++ app/app.js
@@ -1,36 +1,33 @@
-const carListOb = {
-  carlist: [
-    {
-      title: "Cheverloret Camaro",
-      imgUrl:
-        "https://www.chevrolet.ca/content/dam/chevrolet/na/canada/english/index/performance/2022-camaro/mov/new/2022-camaro-masthead-01.jpg?imwidth=960",
-      price: 230000,
-      description: "A better and fast car",
-    },
-    {
-      title: "Hyundai Tucson",
-      imgUrl:
-        "https://www.indiacarnews.com/wp-content/uploads/2022/03/2022-Hyundai-Tucson-7-Seater.jpg",
-      price: 260000,
-      description: "A better and family car",
-    },
-    {
-      title: "Hyundai Tucson",
-      imgUrl:
-        "https://www.indiacarnews.com/wp-content/uploads/2022/03/2022-Hyundai-Tucson-7-Seater.jpg",
-      price: 260000,
-      description: "A better and family car",
-    },
-    {
-      title: "Cheverloret Camaro",
-      imgUrl:
-        "https://www.chevrolet.ca/content/dam/chevrolet/na/canada/english/index/performance/2022-camaro/mov/new/2022-camaro-masthead-01.jpg?imwidth=960",
-      price: 230000,
-      description: "A better and fast car",
-    },
-  ],
+class Car {
+  constructor(title, imgUrl, price, description) {
+    this.title = title;
+    this.imgUrl = imgUrl;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+class CarList {
+  carlist = [
+    new Car(
+      "Cheverloret Camaro",
+      "https://www.chevrolet.ca/content/dam/chevrolet/na/canada/english/index/performance/2022-camaro/mov/new/2022-camaro-masthead-01.jpg?imwidth=960",
+      200000,
+      "A better and fast car"
+    ),
+    new Car(
+      "Hyundai Tucson",
+      "https://www.indiacarnews.com/wp-content/uploads/2022/03/2022-Hyundai-Tucson-7-Seater.jpg",
+      203000,
+      "A better and family car"
+    ),
+  ];
+
+  // todo create the class called CarElement and then
+  // todo  paste the car element inside then call it inside the carList
+
   render() {
-    const rootApp = document.getElementById("app");
+    const rootApp = document.querySelector("#app");
     const header = document.createElement("div");
     header.className = "header";
     header.innerHTML = `
@@ -45,7 +42,7 @@ const carListOb = {
     for (let car of this.carlist) {
       const carItem = document.createElement("li");
       carItem.innerHTML = `
-      <div  >
+      <div>
       <div class="list-image">
       <img src="${car.imgUrl}" alt="${car.title}"/>
       </div>
@@ -58,7 +55,8 @@ const carListOb = {
     }
 
     rootApp.append(header, carListDOM);
-  },
-};
+  }
+}
 
-carListOb.render();
+const app = new CarList();
+app.render();
